cb6cf806fcb69aba98a2c4b8aa9b4f424e9f0bb9
uypaolo <uypaolo@gmail.com>
2013-10-20 15:45:58 +0800

    Removed JTextAreas in ViewPanel

--- Linguist'sAssistant-Group6/src/View/ViewPanel.java
+++ Linguist'sAssistant-Group6/src/View/ViewPanel.java
@@ -22,10 +22,6 @@ public class ViewPanel extends JPanel{
 	private JScrollPane dPane;
 	private JScrollPane cPane;
 	
-	private JTextArea ft;
-	private JTextArea dt;
-	private JTextArea ct;
-	
 	public ViewPanel(){
 		this.c = null;
 		this.setLayout(new MigLayout());
@@ -36,33 +32,14 @@ public class ViewPanel extends JPanel{
 		this.tPane = new JTabbedPane();
 		//this.tPane.setLayout(new MigLayout());
 		
-		this.ft = new JTextArea();
-		this.ft.setText("");
-		this.ft.setEditable(false);
-		
-		this.dt = new JTextArea();
-		this.dt.setText("");
-		this.dt.setEditable(false);
-		
-		this.ct = new JTextArea();
-		this.ct.setText("");
-		this.ct.setEditable(false);
-		
 		this.details = new JPanel();
 		this.details.setLayout(new MigLayout());
-		this.details.add(this.dt);
 		
 		this.features = new JPanel();
 		this.features.setLayout(new MigLayout());
-		this.features.add(this.ft);
 		
 		this.concepts = new JPanel();
 		this.concepts.setLayout(new MigLayout());
-		this.concepts.add(this.ct);
-		
-		this.dt.setBackground(details.getBackground());
-		this.ft.setBackground(features.getBackground());
-		this.ct.setBackground(features.getBackground());
 		
 		this.fPane = new JScrollPane(this.features);
 		this.dPane = new JScrollPane(this.details);
@@ -76,33 +53,9 @@ public class ViewPanel extends JPanel{
 	}
 	
 	public void resetPanels(){
-		this.ft.setText("");
-		this.ct.setText("");
-		this.dt.setText("");
+
 	}
 	
-	public void setDetails(String det){
-		//this.dt.
-		this.dt.setText(det);
-	}
-	
-	public void setFeatures(String feat){
-		if(feat.trim().equals("")){
-			this.ft.setText("No Feature");
-		}
-		else{
-			this.ft.setText(feat);
-		}
-	}
-	
-	public void setConcepts(String con){
-		if(con.trim().equals("")){
-			this.ct.setText("No Concept");
-		}
-		else{
-			this.ct.setText(con);
-		}
-	}
 	
 	public void setConst(constContainer cn){
 		if(c != null){
