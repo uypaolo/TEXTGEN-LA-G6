204733a44f56f25332bcdb145aa0af0722067aa6
uypaolo <uypaolo@gmail.com>
2013-10-20 14:40:07 +0800

    Added details in AddBlockWindow.java

--- Linguist'sAssistant-Group6/src/View/AddBlockWindow.java
+++ Linguist'sAssistant-Group6/src/View/AddBlockWindow.java
@@ -1,6 +1,15 @@
 package View;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class AddBlockWindow extends JFrame{
+	
+	private JLabel pos;
+	private JLabel concept;
+	
+	public AddBlockWindow(){
+		
+	}
+	
 }
